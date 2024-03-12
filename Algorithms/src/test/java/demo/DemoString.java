8cff7026a68d29d6802e2986342a4e6ea25e57a1
matthcol <matthias.colin@gmail.com>
2024-03-12 13:09:37 +0100

    exo2 solution

--- Algorithms/src/test/java/demo/DemoString.java
+++ Algorithms/src/test/java/demo/DemoString.java
@@ -19,9 +19,13 @@ public class DemoString {
     void exo1(){
         String text = "Montauban, la ville des boulets";
         // trouver la position de la première virgule
+        int index = text.indexOf(",");
         // recupérer le début du texte jusqu'à la virgule
+        String city = text.substring(0, index);
         // le mettre en majuscule
+        String cityUpperCase = city.toUpperCase();
         // l'afficher
+        System.out.println(cityUpperCase);
     }
 
     @ParameterizedTest
@@ -32,9 +36,18 @@ public class DemoString {
             "texte non conforme"
     })
     void exo2(String text){
-        // idem exo1, en tenant compte du cas
-        // où il n'y a pas de virgule
-        //      => afficher message erreur
+        // trouver la position de la première virgule
+        int index = text.indexOf(",");
+        if (index < 0){
+            System.out.println("texte non conforme, il manque une virgule");
+        } else {
+            // recupérer le début du texte jusqu'à la virgule
+            String city = text.substring(0, index);
+            // le mettre en majuscule
+            String cityUpperCase = city.toUpperCase();
+            // l'afficher
+            System.out.println(cityUpperCase);
+        }
     }
 
 }
