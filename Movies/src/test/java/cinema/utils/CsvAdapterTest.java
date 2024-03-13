4deb0efc2cd90540ced739552324d4c0fb00076b
matthcol <matthias.colin@gmail.com>
2024-03-13 16:12:31 +0100

    add test empty duration

--- Movies/src/test/java/cinema/utils/CsvAdapterTest.java
+++ Movies/src/test/java/cinema/utils/CsvAdapterTest.java
@@ -16,4 +16,13 @@ class CsvAdapterTest {
         assertEquals(50, movie.getDuration());
     }
 
+    @Test
+    void testFromLine_durationEmpty(){
+        String line = "5257,Excuse Me,1915,,\"Henry Mallory, U.S.A., receives orders to join his regiment which is to embark for the Philippines. The Overland Limited is the only train that will enable him to reach the coast in time to escape a court-martial. Having a little time to spare he persuades Marjorie to elope with him and reserves two berths. They rea...";
+        var movie = CsvAdapter.fromLine(line);
+        assertEquals("Excuse Me", movie.getTitle());
+        assertEquals(1915, movie.getYear());
+        assertNull(movie.getDuration());
+    }
+
 }
