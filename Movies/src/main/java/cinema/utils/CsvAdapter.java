4058079ea931a0ce34a2673ea5bc4c64447f4c4a
matthcol <matthias.colin@gmail.com>
2024-03-13 16:21:41 +0100

    fix movieFromLine

--- Movies/src/main/java/cinema/utils/CsvAdapter.java
+++ Movies/src/main/java/cinema/utils/CsvAdapter.java
@@ -6,15 +6,16 @@ import java.util.Arrays;
 
 public class CsvAdapter {
 
-    public static Movie fromLine(String line){
+    public static Movie movieFromLine(String line){
         var words = line.split(",");
+        // code inutile (juste pour jouer avec les streams)
         var infos = Arrays.stream(words)
                 .limit(4)
                 .toList();
         return new Movie(
                 infos.get(1),
                 Integer.parseInt(infos.get(2)),
-                Integer.parseInt(infos.get(3))
+                infos.get(3).isEmpty() ? null : Integer.parseInt(infos.get(3))
         );
     }
 }
